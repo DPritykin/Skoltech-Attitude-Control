2033711ed79777ff016ccdf79ddf7bd572308d36
BismaSajid <103048572+BismaSajid@users.noreply.github.com>
2023-06-29 16:03:00 +0300

    Update MTMGyroBasedFilterExample.m

--- examples/MTMGyroBasedFilterExample.m
+++ examples/MTMGyroBasedFilterExample.m
@@ -75,6 +75,116 @@ sim.setSatellite(sat);
 sim.setFilter(ekf);
 
 %% simulation loop
-simResults = sim.run('fullmagneticcontrol');
+number_of_runs = 10;
+RMSE = zeros(6, number_of_runs);
+RMSE_EKF = zeros(6, number_of_runs);
 
-sim.plotResults(simResults);
+% for i =1:number_of_runs
+
+    [simResults,ekfResults] = sim.run('fullMagneticControl');
+
+    plotResults(simResults, sim.orb.meanMotion);    
+    comparison_plots(simResults,ekfResults,sim.orb.meanMotion); % EKF Results & True and Estimated Results comparison
+
+%     RMSE(:, i) = plotResults(simResults(i), sim.orb.meanMotion);
+%     RMSE_EKF(:,i) = comparison_plots(simResults(i), ekfResults(i), sim.orb.meanMotion);
+% 
+% end 
+
+%% plotter
+function rmse = plotResults(simData, meanMotion)
+    [pitch, roll, yaw] = quat2angle(simData(2:5, 1:end)', 'YXZ');
+    
+    pitch = rad2deg(pitch);
+    roll = rad2deg(roll);
+    yaw = rad2deg(yaw);
+    
+    len = length(pitch);
+    startIndex = round(len * 3 / 4);
+    
+    ref0 = zeros(len, 1);
+
+    simData(6:8,1:end) = simData(6:8,1:end) *(180/pi); % rad/s to deg/s
+    
+    rmseRoll = sqrt(immse(roll(startIndex:end), ref0(startIndex:end)));
+    rmsePitch = sqrt(immse(pitch(startIndex:end), ref0(startIndex:end)));
+    rmseYaw = sqrt(immse(yaw(startIndex:end), ref0(startIndex:end)));
+    rmseW1 = sqrt(immse(simData(6, startIndex:end)', ref0(startIndex:end)));
+    rmseW2 = sqrt(immse(simData(7, startIndex:end)' - meanMotion, ref0(startIndex:end)));
+    rmseW3 = sqrt(immse(simData(8, startIndex:end)', ref0(startIndex:end)));
+    
+    red = [203/255, 37/255, 37/255];
+    green = [138/255, 181/255, 73/255];
+    blue = [33/255, 144/255, 209/255];
+
+    timeInHours = simData(1, :) / 3600;
+    
+    figure
+    subplot(2, 3, 1)
+    plot(timeInHours, pitch, 'Color', red, 'LineWidth', 2)
+    hold on
+    plot(timeInHours, roll, 'Color', green, 'LineWidth', 2)
+    hold on
+    plot(timeInHours, yaw, 'Color', blue, 'LineWidth', 2)
+    grid on
+    xlabel('Time in hours')
+    ylabel('Euler Angles, [deg]')
+    legend('pitch','roll','yaw');
+    
+    subplot(2, 3, 4)
+    plot(timeInHours(startIndex:end), pitch(startIndex:end), 'Color', red, 'LineWidth', 2)
+    hold on
+    plot(timeInHours(startIndex:end), roll(startIndex:end), 'Color', green, 'LineWidth', 2)
+    hold on
+    plot(timeInHours(startIndex:end), yaw(startIndex:end), 'Color', blue, 'LineWidth', 2)
+    grid on
+    xlabel('Time in hours')
+    ylabel('Euler Angles Errors, [deg]')
+    legend_p = ['RMSE_{pitch} = ', num2str(rmsePitch, '%10.2e\n')];
+    legend_r = ['RMSE_{roll} = ', num2str(rmseRoll, '%10.2e\n')];
+    legend_y = ['RMSE_{yaw} = ', num2str(rmseYaw, '%10.2e\n')];
+    legend(legend_p, legend_r, legend_y);
+    
+    subplot(2, 3, 2) % angular velocity
+    plot(timeInHours, simData(6, 1:end), 'Color', red, 'LineWidth', 2)
+    hold on
+    plot(timeInHours, simData(7, 1:end), 'Color', green, 'LineWidth', 2)
+    hold on
+    plot(timeInHours, simData(8, 1:end), 'Color', blue, 'LineWidth', 2)
+    grid on
+    xlabel('Time in hours')
+    ylabel('Angular Velocity Components, [deg/sec]')
+    legend('\omega_1','\omega_2','\omega_3');
+    
+    subplot(2, 3, 5) % angular velocity
+    plot(timeInHours(startIndex:end), simData(6, startIndex:end), 'Color', red, 'LineWidth', 2)
+    hold on
+    plot(timeInHours(startIndex:end), (simData(7, startIndex:end) - meanMotion), 'Color', green, 'LineWidth', 2)
+    hold on
+    plot(timeInHours(startIndex:end), simData(8, startIndex:end), 'Color', blue, 'LineWidth', 2)
+    grid on
+    xlabel('Time in hours')
+    ylabel('Angular Velocity Errors, [deg/sec]')
+    legend_1 = ['RMSE_{\omega_1} = ', num2str(rmseW1, '%10.2e\n')];
+    legend_2 = ['RMSE_{omega_2} = ', num2str(rmseW2, '%10.2e\n')];
+    legend_3 = ['RMSE_{\omega_3} = ', num2str(rmseW3, '%10.2e\n')];
+    legend(legend_1, legend_2, legend_3);
+    
+    subplot(2, 3, 3) % quaternion
+    plot(timeInHours, simData(2, 1:end), 'k', 'LineWidth', 2)
+    hold on
+    plot(timeInHours, simData(3, 1:end), 'Color', red, 'LineWidth', 2)
+    hold on
+    plot(timeInHours, simData(4, 1:end), 'Color', green, 'LineWidth', 2)
+    hold on
+    plot(timeInHours, simData(5, 1:end), 'Color', blue, 'LineWidth', 2)
+    grid on
+    xlabel('Time in hours')
+    ylabel('Quaternion Components')
+    legend('q0','q1','q2','q3');
+
+    sgtitle('ADCS Simulation: Magnetic Control')
+
+    rmse = [rmseRoll rmsePitch rmseYaw rmseW1 rmseW2 rmseW3];
+
+end
