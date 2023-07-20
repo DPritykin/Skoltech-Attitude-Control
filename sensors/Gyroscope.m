3e90ff5889ee7b325df7ffb01807a4771f0585dd
BismaSajid <103048572+BismaSajid@users.noreply.github.com>
2023-07-20 11:58:42 +0300

    Added function for bias generation

--- sensors/Gyroscope.m
+++ sensors/Gyroscope.m
@@ -1,6 +1,10 @@
 classdef Gyroscope < AbstractSensor
 
     methods
+        function val = getGyroBias(this)
+            val = randn(3,1); % randomly initialized constant bias
+        end
+        
         function val = getSensorReadings(this, trueValue)
             % trueValue - angular velocity in the satellite body frame
             generatedNoise = normrnd(this.bias, this.noiseSigma, [3, 1]);
