b53b7e2cda4909d41ba84a62e513ec747377d5c6
BismaSajid <103048572+BismaSajid@users.noreply.github.com>
2023-07-24 18:01:56 +0300

    Update Gyroscope.m

--- sensors/Gyroscope.m
+++ sensors/Gyroscope.m
@@ -3,6 +3,10 @@ classdef Gyroscope < AbstractSensor
     methods
         function val = getGyroBias(this)
             val = randn(3,1); % randomly initialized constant bias
+            if any(abs(val)) > 1e-5
+                 maxRatio = max(abs(val) ./ 1e-5);
+                 val = val / maxRatio;
+            end
         end
         
         function val = getSensorReadings(this, trueValue)
