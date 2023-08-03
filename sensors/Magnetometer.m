3b2eb145c9c10cbeb9bb79e1316b3df0ea58ba4e
Hooriya9 <131167623+Hooriya9@users.noreply.github.com>
2023-08-03 04:42:03 +0300

    Add files via upload

--- sensors/Magnetometer.m
+++ sensors/Magnetometer.m
@@ -5,7 +5,7 @@ classdef Magnetometer < AbstractSensor
             % trueValue - magnetic field in the satellite body frame
             generatedNoise = normrnd(this.bias, this.noiseSigma, [3, 1]);
 
-            val = trueValue + this.dcm * generatedNoise;
+            val = trueValue + this.dcm * generatedNoise; 
             intM = 0; 
         end
     end
--- sensors/SunSensor.m
+++ sensors/SunSensor.m
@@ -3,58 +3,31 @@ classdef SunSensor < AbstractSensor
     methods
         function [val,intensity] = getSensorReadings(this, trueValue)
             % trueValue - sun vector in the body frame 
-            generatedNoise = normrnd(this.bias, this.noiseSigma, [3, 1]);
 
-            random_orthogonal = rand(3, 1); % Generating a random orthogonal vector for rotation axis
-            random_orthogonal = random_orthogonal/norm(random_orthogonal);
+            randomVector = rand(3, 1);
+            eAxis = cross(trueValue, randomVector);
+            eAxis = eAxis / norm(eAxis);
+            
+            rotationAngle = normrnd(0, this.noiseSigma);
+            
+            % As all the three axes are assumed to have the same sigma 
+            rotationQuaternion = [cos(rotationAngle(1)/2); eAxis*sin(rotationAngle(1)/2)];
 
-            rotation_axis = cross(trueValue, random_orthogonal);
-            rotation_axis = rotation_axis / norm(rotation_axis);
+            rotatedVector = quatRotate(rotationQuaternion, trueValue);
+            val = rotatedVector + this.bias;
 
-            cos_angles = cos(generatedNoise);
-            sin_angles = sin(generatedNoise);
+            sunSensorNormal = [0; 0; -1];
+            cos_incident_angle = dot(rotatedVector, sunSensorNormal);
 
-            % Extract individual angles for x, y, and z components
-            cos_angle_x = cos_angles(1);
-            cos_angle_y = cos_angles(2);
-            cos_angle_z = cos_angles(3);
-            
-            sin_angle_x = sin_angles(1);
-            sin_angle_y = sin_angles(2);
-            sin_angle_z = sin_angles(3);
-            
-            % Create the 3x3 skew-symmetric matrices for the rotation axes
-            Sx = [0, -rotation_axis(3), rotation_axis(2);
-                  rotation_axis(3), 0, -rotation_axis(1);
-                  -rotation_axis(2), rotation_axis(1), 0];
-            
-            Sy = [0, -rotation_axis(3), rotation_axis(2);
-                  rotation_axis(3), 0, -rotation_axis(1);
-                  -rotation_axis(2), rotation_axis(1), 0];
-            
-            Sz = [0, -rotation_axis(3), rotation_axis(2);
-                  rotation_axis(3), 0, -rotation_axis(1);
-                  -rotation_axis(2), rotation_axis(1), 0];
-            
-            % Compute the rotation matrices for x, y, and z axes using Rodrigues' rotation formula
-            Rx = eye(3) + sin_angle_x * Sx + (1 - cos_angle_x) * (Sx * Sx);
-            Ry = eye(3) + sin_angle_y * Sy + (1 - cos_angle_y) * (Sy * Sy);
-            Rz = eye(3) + sin_angle_z * Sz + (1 - cos_angle_z) * (Sz * Sz);
-            
-            % Compute the overall rotation matrix R_noise by combining the rotations about x, y, and z axes
-            R_noise = Rx * Ry * Rz; 
-            val = R_noise * trueValue;
-
-            %INCIDENT ANGLE CALCULATION
-            sunSensorNormal = this.position/1e-2;
-            cos_incident_angle = dot(val, sunSensorNormal);
-            incident_angle_deg = acosd(cos_incident_angle);
-
-            if incident_angle_deg < this.fov
-                intensity = 1;
+            % Calculating intensity (proportional to the incident angle cosine when in fov)
+            if cos_incident_angle >= cos(this.fov)
+                intensity = cos_incident_angle;
             else
                 intensity = 0;
-            end 
+            end
         end
     end
 end
+
+
+          
